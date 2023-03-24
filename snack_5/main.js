dfc167dc020f1b4783f6f1bb02478045e024a443
Richter96 <castiglione96.rc@gmail.com>
2023-03-24 12:49:04 +0100

    bo

---
+++ snack_5/main.js
@@ -0,0 +1,19 @@
+/* 
+Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" 
+*/
+
+Array nomi
+
+function myfunction(...parametri) {
+    console.log(parametri)
+}
+
+
+
+myfunction([
+    'clsudio',
+    'giuseppe',
+    'riccardo'
+], 2, 3)
