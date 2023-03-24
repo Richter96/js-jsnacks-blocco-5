ea77b525c848c89dca381782e136b6efa6497100
Richter96 <castiglione96.rc@gmail.com>
2023-03-24 12:18:40 +0100

    add sum zucchine

--- snack_1/main.js
+++ snack_1/main.js
@@ -7,29 +7,42 @@ Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna
 
 const zucchine = [
     {
-        varietà: tonda,
+        varietà: 'tonda',
         lunghezza: 10,
         peso: 0.1
-    }
+    },
     {
-        varietà: trombetta,
+        varietà: 'trombetta',
         lunghezza: 10,
         peso: 0.5
-    }
+    },
     {
-        varietà: napoletana,
+        varietà: 'napoletana',
         lunghezza: 10,
         peso: 0.3
-    }
+    },
     {
-        varietà: tonda,
+        varietà: 'tonda',
         lunghezza: 10,
         peso: 0.4
-    }
+    },
     {
-        varietà: trombetta,
+        varietà: 'trombetta',
         lunghezza: 10,
         peso: 0.2
     }
 ]
 
+// calcoliamo quanto pesano tutte le zucchine
+
+let sumZucchine = 0
+
+// creo un ciclo per prendere elementi peso
+zucchine.forEach(element => {
+    const {peso} = element
+    console.log(peso)
+
+    sumZucchine += peso
+});
+
+console.log(sumZucchine)
