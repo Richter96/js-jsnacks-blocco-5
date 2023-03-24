ce16eaa4982d8345ae0841c27e7875e52fc3d16a
Richter96 <castiglione96.rc@gmail.com>
2023-03-24 12:10:04 +0100

    add obgect

---
+++ snack_1/main.js
@@ -0,0 +1,35 @@
+/* 
+Snack 1:
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine. 
+*/
+
+// creo un array di 1o oggetti
+
+const zucchine = [
+    {
+        varietà: tonda,
+        lunghezza: 10,
+        peso: 0.1
+    }
+    {
+        varietà: trombetta,
+        lunghezza: 10,
+        peso: 0.5
+    }
+    {
+        varietà: napoletana,
+        lunghezza: 10,
+        peso: 0.3
+    }
+    {
+        varietà: tonda,
+        lunghezza: 10,
+        peso: 0.4
+    }
+    {
+        varietà: trombetta,
+        lunghezza: 10,
+        peso: 0.2
+    }
+]
+
